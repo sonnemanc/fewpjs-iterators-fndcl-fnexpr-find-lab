7ac43ab47977121f1b52048de70072cf36a902f6
Corban <sonnemanc@gmail.com>
2022-02-08 13:17:47 -0800

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+function superbowlWin(a) {
+    let result = a.find( a => a.result === 'W')
+    return !!result ? result.year : undefined    // IF result is truthy, THEN return result.year ELSE return undefined
+}
